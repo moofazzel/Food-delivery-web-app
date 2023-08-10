fcbf57abf924bd97d2ca30695a094be73fd85fd4
jakia12 <owasi110@gmail.com>
2023-08-10 11:52:50 +0600

    tailwind config modified

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,41 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+
+
+  ],
   theme: {
     extend: {},
+    screens: {
+      xs: '480px',
+      sm: '768px',
+      md: '992px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    container: {
+      center: true,
+    },
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+    }
+    ,
+
+    colors: {
+      primaryCol: "#DA3C3C",
+      secondaryCol: "#C13030",
+      textCol: "#191919"
+
+
+    },
   },
+
   plugins: [],
-};
+
+
+}
