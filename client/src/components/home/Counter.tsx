5da8c254e2adfbbd4e1499cbc7c8462948a22ad1
shathipaul <shathipaul11gmail.com>
2023-08-12 01:49:16 +0600

    added counter

---
+++ client/src/components/home/Counter.tsx
@@ -0,0 +1,58 @@
+import CountUp from 'react-countup';
+import img1 from '../../assets/countIcons/icon-1.png'
+import img2 from '../../assets/countIcons/icon-2.png'
+import img3 from '../../assets/countIcons/icon-3.png'
+import img4 from '../../assets/countIcons/icon-4.png'
+
+interface ICountData {
+    id: number;
+    img: string;
+    text: string;
+    countEnd: number;
+}
+
+const Counter = () => {
+    const countData: ICountData[] = [
+        { id: 1, img: img1, text: 'Menu Types', countEnd: 200 },
+        { id: 2, img: img2, text: 'Different Origin', countEnd: 50 },
+        { id: 3, img: img3, text: 'Pasta & Noodles', countEnd: 850 },
+        { id: 4, img: img4, text: 'Meals To Go', countEnd: 580 },
+    ];
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-20 gap-4'>
+            {
+                countData.map(data => <div
+                    key={data.id}
+                    className='flex flex-col items-center py-4'
+                >
+                    <img
+                        src={data.img}
+                        loading='lazy'
+                        width={60}
+                        alt=''
+                    />
+                    <p
+                        className='text-2xl font-semibold text-gray py-4'>
+                        {data.text}
+                    </p>
+                    <CountUp
+                        start={0}
+                        end={data.countEnd}
+                        delay={0}>
+                        {({ countUpRef }) => (
+                            <div>
+                                <span
+                                    className='text-3xl font-semibold text-primary'
+                                    ref={countUpRef} />
+                            </div>
+                        )}
+                    </CountUp>
+                </div>)
+            }
+
+        </div>
+
+    );
+};
+
+export default Counter;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import Counter from '../../components/home/Counter';
 import SimpleAndDeliciousFood from './SimpleAndDeliciousFood';
 
 const Home = () => {
   return (
     <div>
       <SimpleAndDeliciousFood />
+      <Counter />
     </div>
   );
 };
