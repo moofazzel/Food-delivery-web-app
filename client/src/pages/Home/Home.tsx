f9af57192bb0ea57044b5f5d9fe5dd332336f1b7
moofazzel <2192461016@uttarauniversity.edu.bd>
2023-08-12 00:48:12 +0600

    Added upgrade to the latest taste

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import UpgradeToLatestTest from '../../components/home/UpgradeToLatestTest';
 import SimpleAndDeliciousFood from './SimpleAndDeliciousFood';
 
 const Home = () => {
   return (
     <div>
+      <UpgradeToLatestTest/>
       <SimpleAndDeliciousFood />
     </div>
   );
