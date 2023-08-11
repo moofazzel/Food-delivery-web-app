ca7d0afc4c19927b0a76bdf9b8208659a3d61af0
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-08-11 13:34:02 +0600

    Simple And Delicious Food Section Completed

--- client/src/pages/Home/SimpleAndDeliciousFood.tsx
+++ client/src/pages/Home/SimpleAndDeliciousFood.tsx
@@ -1,11 +1,38 @@
+import banner from '../../assets/SimpleAndDeliciousFood/banner.webp';
+import paper from '../../assets/Common/paper-design.webp';
+
 const SimpleAndDeliciousFood = () => {
   return (
-    <div>
-      <p>Enjoy Great Recipe</p>
-      <h2>Simple And Delicious Food</h2>
-      <h4>Flat 20% Discount</h4>
-      <button>Shop Now</button>
-    </div>
+    <>
+      <img src={paper} alt="Paper Design" className="mb-[-20px]" />
+      <div
+        className="h-[500px] font-sans"
+        style={{
+          backgroundImage: `linear-gradient(#00000010, #00000010), url(${banner})`,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      >
+        <div className="container flex items-center justify-center h-full text-center">
+          <div>
+            <p className="text-xl font-semibold text-black mb-5">
+              Enjoy Great Recipe
+            </p>
+            <h2 className="font-semibold text-[40px] lg:text-5xl xl:text-6xl mb-6 text-primary">
+              Simple And Delicious Food
+            </h2>
+            <h4 className="text-4xl lg:font-semibold text-black mb-5">
+              Flat 20% Discount
+            </h4>
+            <button className="bg-primary text-white px-4 py-[10px] text-lg font-semibold mt-3 hover:bg-secondary duration-300">
+              SHOP NOW
+            </button>
+          </div>
+        </div>
+      </div>
+      <img src={paper} alt="Paper Design" className="mt-[-20px]" />
+    </>
   );
 };
 
