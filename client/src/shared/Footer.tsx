f1897c18fd565ecfbfac84004f4127ef08a3548a
rafsan3503 <rafsanchowdhuryrudro@gmail.com>
2023-08-17 15:28:29 +0600

    first commit

--- client/src/shared/Footer.tsx
+++ client/src/shared/Footer.tsx
@@ -1,24 +1,56 @@
 
 import './Footer.css';
+import HomeIcon from '@mui/icons-material/Home';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
 const Footer = () => {
-  // const footerColumns = [
-  //   {
-  //     title: 'Column 1',
-  //     content: ['Link 1', 'Link 2', 'Link 3'],
-  //   },
-  //   {
-  //     title: 'Column 2',
-  //     content: ['Link 4', 'Link 5', 'Link 6'],
-  //   },
-  //   {
-  //     title: 'Column 3',
-  //     content: ['Link 7', 'Link 8', 'Link 9'],
-  //   },
-  //   {
-  //     title: 'Column 4',
-  //     content: ['Link 10', 'Link 11', 'Link 12'],
-  //   },
-  // ];
+  const footerCols = [
+    {
+      id: 1,
+      title: 'Help',
+      content: [{ id: 1, item: "Search" },
+      { id: 2, item: "Help" },
+      { id: 3, item: "Information" },
+      { id: 4, item: "Privacy Policy" },
+      { id: 5, item: "Shipping Details" },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Support',
+      content: [{ id: 1, item: "Contact" },
+      { id: 2, item: "About" },
+      { id: 3, item: "Refund" },
+      { id: 4, item: "Delivery" },
+
+      ],
+
+    },
+    {
+      id: 3,
+      title: 'Information',
+      content: [{ id: 1, item: "Search Term" },
+      { id: 2, item: "Advanced Search" },
+      { id: 3, item: "Faqs and Help" },
+      { id: 4, item: "Store Location" },
+
+      ],
+
+    },
+    {
+      id: 4,
+      title: 'Contact Us',
+
+      content: [
+        { id: 1, item: " 58 A, East Madison Street, Baltimore, MD, USA 4508", icon: HomeIcon },
+        { id: 2, item: " 0000 - 123456789", icon: PhoneIcon },
+        { id: 3, item: " mail@example.com, MD, USA 4508", icon: EmailIcon },
+        { id: 4, item: " 9.30AM - 7.30PM", icon: AccessTimeIcon },
+      ],
+    },
+  ];
 
   return (
     <footer className="footer_bg">
@@ -36,6 +68,35 @@ const Footer = () => {
           </div>
         ))}
       </div> */}
+
+
+      <div className="flex items-center justify-center gap-6">
+        {
+          footerCols.map((col) => (
+            <div className="w-full md:w-6/12 lg:w-4/12 mt-2" key={col.id}>
+              <h2 className="mb-4 text-sm font-semibold text-[#f7f7f7] capitalize dark:text-white">
+                {col.title}
+              </h2>
+              {
+                col.content.map((mItem) => (
+                  <ul key={mItem.id} className="text-lightTextColor flex flex-col gap-y-2 text-sm font-normal capitalize mb-6">
+                    <li>
+
+                      <a className="hover:underline">
+                        {mItem.item}
+                      </a>
+                    </li>
+
+
+                  </ul>
+                ))
+              }
+
+            </div>
+          ))
+        }
+
+      </div>
     </footer>
   );
 };
